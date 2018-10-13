17808e227b3073d38ed4cb2257efe6b90d240fc8
bigredwill <bigredwill@gmail.com>
2018-10-13 15:37:00 -0700

    Let nav breathe, move links to right side.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ const routes = [
 const linkStyles = 'link dim nav-blue f5 f4-m f4-l dib mr2 v-btm'
 // function to determine if link is active or deeper routes
 const isPartiallyActive = ({ isPartiallyCurrent, location, href }) => {
-  // debugger;
+  // don't highlight top level route for every page
   return (isPartiallyCurrent && href !== '/') ||
     (href === '/' && location.pathname === '/')
     ? { className: `${linkStyles} b ul` }
@@ -27,11 +27,11 @@ const isPartiallyActive = ({ isPartiallyCurrent, location, href }) => {
 }
 
 const Navbar = ({ route = '/' }) => (
-  <nav className="measure-wide center pt4 sans-serif">
+  <nav className="measure-wide center pt4 sans-serif flex">
     <div className="tracked mid-gray dib f5 f4-m f4-l v-btm mr2 mb2-s">
       Will Simons
     </div>
-    <div className="dib mt3-s">
+    <div className="dib mt3-s ml-auto">
       {routes.map(obj => {
         return (
           <Link
